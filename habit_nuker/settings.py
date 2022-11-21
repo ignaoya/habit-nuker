5d0d916fc2c66437e25ef9fc4972731acafb9605
nicky-eng <nicolasoyarzabal@gmail.com>
2022-11-21 14:03:09 +0100

    Startapp and add job_hunter app to settings.

--- habit_nuker/settings.py
+++ habit_nuker/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'tracker.apps.TrackerConfig',
+    'tracker',
+    'job_hunter',
 ]
 
 MIDDLEWARE = [
