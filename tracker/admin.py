934a5d258f704ee30676d9a203c15441445607d5
nicky-eng <nicolasoyarzabal@gmail.com>
2022-11-21 17:30:53 +0100

    Combine Habit and HabitGoal models, add Challenge

--- tracker/admin.py
+++ tracker/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 
-from .models import NukeUser
+from .models import NukeUser, Category, Habit, Challenge
+
 
 admin.site.register(NukeUser)
-
+admin.site.register(Category)
+admin.site.register(Habit)
+admin.site.register(Challenge)
 
--- tracker/models.py
+++ tracker/models.py
@@ -3,26 +3,39 @@ from django.contrib.auth.models import AbstractUser
 
 
 class NukeUser(AbstractUser):
-    name = models.CharField(max_length=100)
+    pass
 
 
 class Category(models.Model):
     name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.name}"
 
 
 class Habit(models.Model):
     name = models.CharField(max_length=50)
     user = models.ForeignKey(NukeUser, on_delete=models.CASCADE)
     category = models.ForeignKey(Category, on_delete=models.SET_NULL,
-        null=True, blank=True)
-    measure_of_completion = models.IntegerField(default=1)
-
-class HabitGoal(models.Model):
-    name = models.CharField(max_length=50)
-    description = models.TextField()
-    habit = models.OneToOneField(Habit, on_delete=models.CASCADE)
+       null=True, blank=True)
+    goal_description = models.TextField()
     quantitative_goal = models.IntegerField(default=100)
     quantitative_goal_units = models.CharField(max_length=50, default="%")
+    measure_of_completion = models.IntegerField(default=1)
+    
+    def __str__(self):
+        return f"{self.name} - {self.user}"
+
+
+class Challenge(models.Model):
+    name = models.CharField(max_length=200)
+    user_habits = models.ManyToManyField(Habit)
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+    
 
 
 
