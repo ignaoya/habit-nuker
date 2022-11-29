d6ec286d0b65c4ea48ee2336e059f9113009e24b
Ignacio Oyarzabal <ignaoya93@gmail.com>
2022-11-29 12:18:01 -0300

    Add auth0 settings and urls, bugs need debuggin. WIP

--- habit_nuker/settings.py
+++ habit_nuker/settings.py
@@ -10,10 +10,13 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
+from dotenv import load_dotenv, find_dotenv
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+TEMPLATE_DIR = os.path.join(BASE_DIR, "tracker", "templates")
 
 
 # Quick-start development settings - unsuitable for production
@@ -56,7 +59,7 @@ ROOT_URLCONF = 'habit_nuker.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -126,3 +129,13 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 AUTH_USER_MODEL = 'tracker.NukeUser'
+
+# Load environment definition file
+ENV_FILE = find_dotenv()
+if ENV_FILE:
+    load_dotenv(ENV_FILE)
+
+# Load Auth0 application settings into memory
+AUTH0_DOMAIN = os.environ.get("AUTH0_DOMAIN")
+AUTH0_CLIENT_ID = os.environ.get("AUTH0_CLIENT_ID")
+AUTH0_CLIENT_SECRET = os.environ.get("AUTH0_CLIENT_SECRET")
--- tracker/urls.py
+++ tracker/urls.py
@@ -3,6 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.HabitListAPIView.as_view(), name='habit_list'),
+    path('login', views.login, name='login'),
+    path('logout', views.logout, name='logout'),
+    path('callback', views.callback, name='callback'),
     path('<int:id>/', views.HabitRetrieveAPIView.as_view(), name='habit_detail'),
     path('create/', views.HabitCreateAPIView.as_view(), name='habit_create'),
     path('update/<int:id>/', views.HabitRetrieveUpdateAPIView.as_view(), name='habit_update'),
