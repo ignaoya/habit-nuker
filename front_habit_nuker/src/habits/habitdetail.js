42cf199c28b5d5b90e9a1d062332e5d453697f83
Ignacio Oyarzabal <ignaoya93@gmail.com>
2023-01-03 12:41:54 -0300

    Add habitupdate to frontend

--- front_habit_nuker/src/habits/habitdetail.js
+++ front_habit_nuker/src/habits/habitdetail.js
@@ -1,6 +1,19 @@
 import React, { Component } from 'react';
+import HabitUpdate from './habitupdate';
 
 class HabitDetail extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showComponent: false,
+    };
+    this.updateHabitDetails = this.updateHabitDetails.bind(this);
+  }
+
+  updateHabitDetails() {
+    this.setState({ showComponent: true });
+  }
+
     render(){
         const habit = this.props.habitDetail;
         return(
@@ -8,7 +21,13 @@ class HabitDetail extends Component {
                 <h4>Habit N. {habit.id}</h4>
                 <h4>{habit.name}</h4>
                 <h4>Habit Goal is {habit.measure_of_completion}</h4>
-                <h4> ------------------------------------- </h4>
+                <button
+                  style={{ backgroundColor: "white" }}
+                  onClick={() => this.updateHabitDetails()}
+                >
+                  Update
+                </button>
+                {this.state.showComponent ? <HabitUpdate habitUpdate={habit} /> : null}
             </div>
         );
      }
